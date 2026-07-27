d0dca4b86e3570bdcdd1523d0eb9454db465cc89
Somil Jain <somiljaintkg1234@gmail.com>
2026-07-27 11:22:36 +0530

    creating auth.js

---
+++ server/graphql/typeDefs/auth.js
@@ -0,0 +1,49 @@
+const { gql } = require('graphql-tag');
+
+module.exports = gql`
+  enum Role {
+    freelancer
+    client
+    admin
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    role: Role!
+    profilePicture: String
+    isVerified: Boolean!
+    createdAt: String!
+  }
+
+  type AuthPayload {
+    accessToken: String!
+    refreshToken: String!
+    user: User!
+  }
+
+  input RegisterInput {
+    name: String!
+    email: String!
+    password: String!
+    role: Role!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    me: User
+    _health: String!
+  }
+
+  type Mutation {
+    register(input: RegisterInput!): AuthPayload!
+    login(input: LoginInput!): AuthPayload!
+    refreshToken(token: String!): AuthPayload!
+    logout: Boolean!
+  }
+`;
