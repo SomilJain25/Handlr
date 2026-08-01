10da7f578989e56e7e86edd01aafedcabdd5b887
Somil Jain <somiljaintkg1234@gmail.com>
2026-08-01 11:27:09 +0530

    updating app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,25 @@ import { Toaster } from 'react-hot-toast';
 
 import { client } from './graphql/client';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
+import ErrorBoundary from './components/ErrorBoundary';
 import Navbar from './components/Navbar';
 import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Toaster position="top-right" />
-          <Navbar />
-          <AppRoutes />
-        </BrowserRouter>
-      </AuthProvider>
-    </ApolloProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <ApolloProvider client={client}>
+          <AuthProvider>
+            <BrowserRouter>
+              <Toaster position="top-right" />
+              <Navbar />
+              <AppRoutes />
+            </BrowserRouter>
+          </AuthProvider>
+        </ApolloProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
